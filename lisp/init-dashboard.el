19dc6826c64575d1478f19ed9481a44a6b9b81a5
George <george.yoda@googlemail.com>
2019-04-24 15:52:46 +0100

    Moved dashboard to init-dashboard.el file

---
+++ lisp/init-dashboard.el
@@ -0,0 +1,26 @@
+;;; init-dashboard.el --- Displays an intro screen on opening
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package dashboard
+  :if (< (length command-line-args) 2)
+  :preface
+  (defun my/dashboard-banner ()
+    "Sets a dashboard banner including information on package initialization
+     time and garbage collections."
+    (setq dashboard-banner-logo-title
+          (format "Emacs ready in %.2f seconds with %d garbage collections."
+                  (float-time
+                   (time-subtract after-init-time before-init-time)) gcs-done)))
+  (setq dashboard-items '((recents  . 10)))
+  :init
+  (add-hook 'after-init-hook 'dashboard-refresh-buffer)
+  (add-hook 'dashboard-mode-hook 'my/dashboard-banner)
+  :custom (dashboard-startup-banner 'logo)
+  :config (dashboard-setup-startup-hook))
+
+(provide 'init-dashboard)
+
+;;; init-dashboard.el ends here
