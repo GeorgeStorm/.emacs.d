d5355ca965a74ce3feb04bed15eaa939d3f3f622
George <george.yoda@googlemail.com>
2019-04-24 15:51:08 +0100

    Moved which-key popup window to bottom instead of right.

--- lisp/init-which-key.el
+++ lisp/init-which-key.el
@@ -8,7 +8,7 @@
   :init
   (which-key-mode)
   :config
-  (which-key-setup-side-window-right-bottom)
+  (which-key-setup-side-window-bottom)
   (setq which-key-sort-order 'which-key-key-order-alpha
     which-key-side-window-max-width 0.33
     which-key-idle-delay 0.05)
